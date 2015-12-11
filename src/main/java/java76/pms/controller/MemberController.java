2c9e3032cbf8d47e4037bf9cdbe2f6ef7910b69f
KaRyuDo <ho4319@naver.com>
2015-12-11 21:13:40 +0900

    a

--- src/main/java/java76/pms/controller/MemberController.java
+++ src/main/java/java76/pms/controller/MemberController.java
@@ -1,54 +1,109 @@
-/*package java76.pms.controller;
+package java76.pms.controller;
 
-import java.util.regex.Pattern;
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java76.pms.dao.MemberDao;
 import java76.pms.domain.Member;
+import java76.pms.util.MultipartHelper;
 
 @Controller
 @RequestMapping("/member/*")
 public class MemberController {
+  public static final String SAVED_DIR = "/file";
+  
   @Autowired MemberDao memberDao;
   @Autowired ServletContext servletContext;
 
-  @RequestMapping(value="join", method=RequestMethod.GET)
-  public String form() {
-    return "member/join";
+  @RequestMapping("list")
+  public String list(
+      @RequestParam(defaultValue="1") int pageNo,
+      @RequestParam(defaultValue="10") int pageSize,
+        HttpServletRequest request) throws Exception {
+
+    HashMap<String,Object> paramMap = new HashMap<>();
+    paramMap.put("startIndex", (pageNo - 1) * pageSize);
+    paramMap.put("length", pageSize);
+    
+    List<Member> members = memberDao.selectList(paramMap);
+
+    request.setAttribute("members", members);
+
+    return "member/MemberList";
+
   }
   
-  @RequestMapping(value="join", method=RequestMethod.POST)
-  public String add(Member member, Model model){
-    try {
-      String email = member.getEmail();
-      String regex = "\\w+@\\w+\\.\\w+";
-      String password = member.getPassword();
-      String rePassword = member.getRePassword();
+  @RequestMapping(value="add", method=RequestMethod.GET)
+  public String form() {
+    return "member/MemberForm";
+  }
+  
+  @RequestMapping(value="add", method=RequestMethod.POST)
+  public String add(Member member, MultipartFile photofile) throws Exception {
+
+    if (photofile.getSize() > 0) {
+      String newFileName = MultipartHelper.generateFilename(photofile.getOriginalFilename());  
+      File attachfile = new File(servletContext.getRealPath(SAVED_DIR) 
+                                 + "/" + newFileName);
+      photofile.transferTo(attachfile);
+      member.setPhoto(newFileName);
       
-      boolean emailCheck = Pattern.matches(regex, email);
-      
-      if (emailCheck == false) {
-        model.addAttribute("errorCode", "regex");
-        return "member/MemberAuthError";
-      } else if (!(password.equals(rePassword))) {
-        model.addAttribute("errorCode", "pwd");
-        return "member/MemberAuthError";
-      }
-      
-      
-      memberDao.insert(member);
-      
-      return "redirect:../auth/login.do";
-    } catch (Exception e) {
-      model.addAttribute("errorCode", "500");
+    }
+
+    memberDao.insert(member);
+
+    return "redirect:list.do";
+
+  }
+  
+  @RequestMapping("detail")
+  public String detail(int no, Model model) 
+          throws Exception {
+
+    Member member = memberDao.selectOne(no);
+    model.addAttribute("member", member);
+    return "member/MemberDetail";
+  }
+
+  @RequestMapping(value="update", method=RequestMethod.POST)
+  public String post(
+      Member member, MultipartFile photofile, Model model) throws Exception {
+
+    if (photofile.getSize() > 0) {
+      String newFileName = MultipartHelper.generateFilename(photofile.getOriginalFilename());  
+      File attachfile = new File(
+          servletContext.getRealPath(SAVED_DIR) + "/" + newFileName);
+      photofile.transferTo(attachfile);
+      member.setPhoto(newFileName);
+    }
+    
+    if (memberDao.update(member) <= 0) {
+      model.addAttribute("errorCode", "401");
+      return "member/MemberAuthError";
+    } 
+
+    return "redirect:list.do";
+  }
+  
+  @RequestMapping("delete")
+  public String delete(int no, Model model) throws Exception {
+
+    if (memberDao.delete(no) <= 0) {
+      model.addAttribute("errorCode", "401");
       return "member/MemberAuthError";
     }
+    return "redirect:list.do";
   }
-}*/
+}
